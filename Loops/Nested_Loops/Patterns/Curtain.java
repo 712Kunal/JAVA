3d51d5e70a7f8423a62a32c9770b1b471c6d74d4
712Kunal <jadhavkun0@gmail.com>
2026-03-24 01:07:26 +0530

    added curtain pattern

---
+++ Loops/Nested_Loops/Patterns/Curtain.java
@@ -0,0 +1,20 @@
+class Curtain {
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 2 * n; j++) {
+
+                if (j <= n - i + 1 || j > n + i - 1) {
+                    System.out.print("*" + " ");
+                } else {
+                    System.out.print(" " + " ");
+                }
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
