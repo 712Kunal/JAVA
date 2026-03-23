0a83e783bcb18cd2c848d7cd51c895e5937534d9
712Kunal <jadhavkun0@gmail.com>
2026-03-23 23:12:29 +0530

    added NumberedSquare

---
+++ Loops/Nested_Loops/Patterns/NumberedSquare.java
@@ -0,0 +1,16 @@
+class NumberedSquare {
+    public static void main(String[] args) {
+        int n = 4;
+        
+        System.out.println();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(i + " ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+}
