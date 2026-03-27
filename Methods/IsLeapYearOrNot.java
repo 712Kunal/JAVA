884fa03e9c5de82c4403808d33d0323c5ce9aedf
712Kunal <jadhavkun0@gmail.com>
2026-03-27 14:25:30 +0530

    leap years in range in method

---
+++ Methods/IsLeapYearOrNot.java
@@ -0,0 +1,22 @@
+
+class IsLeapYearOrNot {
+    public static void main(String[] args) {
+        int start = 1000;
+
+        System.out.println("Leap Years in between " + start + " and 2000 are: ");
+        System.out.print("{ ");
+        while (start <= 2000) {
+            IsLeapYear(start);
+            start++;
+        }
+        System.out.print(" }");
+    }
+
+    public static void IsLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0) {
+            System.out.print(year + " ,");
+        } else if (year % 400 == 0) {
+            System.out.print(year + " ,");
+        }
+    }
+}
